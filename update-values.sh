86ebf7602020f5ec790010b4197ca0e814b7b76b
Harisha C <harishac@Harishas-MacBook-Pro.local>
2025-02-01 18:31:12 +0530

    modified the pipeline and added new shell script-1

--- update-values.sh
+++ update-values.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Path to the azure-pipelines-app.yml file
-PIPELINE_FILE="/Users/harishac/Documents/Projects/Azure/ToDO-App/todo-app/azure-pipelines-app.yml"
-
 # Path to the dev-values.yml file
-VALUES_FILE="/Users/harishac/Documents/Projects/Azure/ToDO-App/todo-app/helm-chart/todo-app/dev-values.yml"
+VALUES_FILE="helm_chart/todo-app/dev-values.yml"
 
 # Extract the build number from the arguments
 BUILD_NUMBER=$1
@@ -15,16 +12,13 @@ if [ -z "$BUILD_NUMBER" ]; then
 fi
 
 # Update the Docker image tag number in the dev-values.yml file
-sed -i '' "s/tag: .*/tag: $BUILD_NUMBER/" $VALUES_FILE
+sed -i "s/tag: .*/tag: $BUILD_NUMBER/" $VALUES_FILE
 
 echo "Updated Docker image tag to $BUILD_NUMBER in $VALUES_FILE"
 
-# Navigate to the repository directory
-cd /Users/harishac/Documents/Projects/Azure/ToDO-App/todo-app
-
 # Configure Git
-git config --global user.email "your-email@example.com"
-git config --global user.name "your-username"
+git config --global user.email "harishdravid9845@gmail.com"
+git config --global user.name "harishc-biz"
 
 # Add the changes to git
 git add $VALUES_FILE
